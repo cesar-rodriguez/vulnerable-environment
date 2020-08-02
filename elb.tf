f3de3eb57638e97b8605b88e3e3bcfd22a56fed1
Cesar Rodriguez <cesar@accurics.com>
2020-08-01 22:25:45 -0400

    makes elb private

--- elb.tf
+++ elb.tf
@@ -10,8 +10,8 @@ resource "aws_security_group" "public_internet" {
     to_port     = 80
     protocol    = "tcp"
     cidr_blocks = [
-      "0.0.0.0/0"
-      #"10.0.0.0/16"
+      #"0.0.0.0/0"
+      "10.0.0.0/16"
     ]
   }
 }
