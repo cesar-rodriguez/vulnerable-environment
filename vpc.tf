b77def5808878f0ff1198e0526866ce484625472
Cesar Rodriguez <cesar@accurics.com>
2020-08-01 23:32:44 -0400

    removes unecessary output

--- vpc.tf
+++ vpc.tf
@@ -52,8 +52,3 @@ output "vpc_id" {
   description = "The ID of the VPC"
   value       = aws_vpc.vpc.id
 }
-
-output "public_subnets" {
-  description = "The IDs of the public subnets"
-  value       = aws_subnet.public
-}
