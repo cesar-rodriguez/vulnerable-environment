c64ae6b19afbf528d230b09a04c70f741e2400fa
Cesar Rodriguez <cesar@accurics.com>
2020-08-01 23:33:10 -0400

    adds health check and outputs DNS name

--- elb.tf
+++ elb.tf
@@ -16,13 +16,20 @@ resource "aws_security_group" "public_internet" {
   }
 }
 
-
 resource "aws_elb" "public_elb" {
   name = "${local.prefix.value}-public-elb"
 
   subnets         = aws_subnet.public.*.id
   security_groups = ["${aws_security_group.public_internet.id}"]
   instances       = aws_instance.web.*.id
+
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 3
+    target              = "HTTP:80/"
+    interval            = 15
+  }
 
   connection_draining = true
   listener {
@@ -35,4 +42,9 @@ resource "aws_elb" "public_elb" {
   tags = {
     Name = "${local.prefix.value}-public-elb"
   }
+}
+
+output "elb_dns" {
+  description = "The DNS Name of the ELB"
+  value       = aws_elb.public_elb.dns_name
 }
