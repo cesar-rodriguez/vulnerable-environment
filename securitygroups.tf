8cfd31a42e70c5508ff75ad0f45687be17dd476e
Cesar Rodriguez <cesar@accurics.com>
2020-08-04 18:59:19 -0400

    fixes web security group

--- securitygroups.tf
+++ securitygroups.tf
@@ -33,8 +33,8 @@ resource "aws_security_group" "web" {
     to_port   = 80
     protocol  = "tcp"
     cidr_blocks = [
-      "0.0.0.0/0"
-      #"10.0.0.0/16"
+      #"0.0.0.0/0"
+      "10.0.0.0/16"
     ]
   }
   egress {
