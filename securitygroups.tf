3bbeaa5ddf02c120ce05e8345e6c6665d6e41a18
Cesar Rodriguez <cesar@accurics.com>
2020-08-03 22:51:28 -0400

    returns to vulnerable

--- securitygroups.tf
+++ securitygroups.tf
@@ -9,7 +9,10 @@ resource "aws_security_group" "public_internet" {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["10.0.0.0/16"]
+    cidr_blocks = [
+      "0.0.0.0/0"
+      #"10.0.0.0/16"
+    ]
   }
   egress {
     from_port   = 0
