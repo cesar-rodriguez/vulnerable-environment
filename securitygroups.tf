5433b11718f72807c3f0ff17fa694d1fbd7ccb77
Cesar Rodriguez <cesar@accurics.com>
2020-09-23 21:51:02 -0400

    adds ssh access

--- securitygroups.tf
+++ securitygroups.tf
@@ -29,6 +29,15 @@ resource "aws_security_group" "web" {
   vpc_id      = aws_vpc.vpc.id
 
   ingress {
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+    cidr_blocks = [
+      "0.0.0.0/0"
+      #"10.0.0.0/16"
+    ]
+  }
+  ingress {
     from_port = 80
     to_port   = 80
     protocol  = "tcp"
