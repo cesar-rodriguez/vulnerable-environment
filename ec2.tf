aba147cb26d9497538bace1ad8c2628cc9d59f30
Cesar Rodriguez <cesar@accurics.com>
2020-09-23 21:55:46 -0400

    adds ssh access

--- ec2.tf
+++ ec2.tf
@@ -13,10 +13,16 @@ data "aws_ami" "latest_ecs" {
   }
 }
 
+resource "aws_key_pair" "auth" {
+  key_name   = "${local.prefix.value}-key"
+  public_key = file("${local.prefix.value}.pub")
+}
+
 resource "aws_instance" "web" {
   count         = 2
   ami           = data.aws_ami.latest_ecs.image_id
   instance_type = "t2.micro"
+  key_name      = "${aws_key_pair.auth.id}"
 
   vpc_security_group_ids = [aws_security_group.web.id]
   subnet_id              = aws_subnet.public[count.index].id
