4af6d400a61acdecab9c84fdad1b96dcabb018fe
Cesar Rodriguez <cesar@accurics.com>
2021-10-19 21:38:09 -0400

    uses elb module

--- elb.tf
+++ elb.tf
@@ -1,32 +1,14 @@
-resource "aws_elb" "public_elb" {
-  name = "${local.prefix.value}-public-elb"
-
-  subnets         = aws_subnet.public.*.id
-  security_groups = [aws_security_group.public_internet.id]
-  instances       = aws_instance.web.*.id
-
-  health_check {
-    healthy_threshold   = 2
-    unhealthy_threshold = 2
-    timeout             = 3
-    target              = "HTTP:80/index.html"
-    interval            = 15
-  }
-
-  connection_draining = true
-  listener {
-    instance_port     = 80
-    instance_protocol = "http"
-    lb_port           = 80
-    lb_protocol       = "http"
-  }
-
-  tags = {
-    Name = "${local.prefix.value}-public-elb"
-  }
+module "elb" {
+  #source         = "git@github.com:therasec/vulnerable-environment.git//modules"
+  #source         = "github.com/therasec/vulnerable-environment//modules"
+  source         = "./modules"
+  elb_name       = "${local.prefix.value}-public-elb"
+  subnets        = aws_subnet.public.*.id
+  security_group = aws_security_group.public_internet.id
+  instances      = aws_instance.web.*.id
 }
 
 output "elb_dns" {
   description = "The DNS Name of the ELB"
-  value       = aws_elb.public_elb.dns_name
+  value       = module.elb.elb_dns
 }
---
+++ modules/elb.tf
@@ -0,0 +1,37 @@
+variable "subnets" {}
+variable "security_group" {}
+variable "instances" {}
+variable "elb_name" {}
+
+resource "aws_elb" "public_elb" {
+  name = "${local.prefix.value}-public-elb"
+
+  subnets         = var.subnets
+  security_groups = [var.security_group]
+  instances       = var.instances
+
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 3
+    target              = "HTTP:80/index.html"
+    interval            = 15
+  }
+
+  connection_draining = true
+  listener {
+    instance_port     = 80
+    instance_protocol = "http"
+    lb_port           = 80
+    lb_protocol       = "http"
+  }
+
+  tags = {
+    Name = var.elb_name
+  }
+}
+
+output "elb_dns" {
+  description = "The DNS Name of the ELB"
+  value       = aws_elb.public_elb.dns_name
+}
