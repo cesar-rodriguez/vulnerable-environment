6fecc3ce668ca34c9d6cd537af87515240f99c7d
Accurics <admin@accurics.com>
2022-03-03 09:55:32 -0500

    Auto Remediation By Accurics

--- securitygroups.tf
+++ securitygroups.tf
@@ -6,12 +6,10 @@ resource "aws_security_group" "public_internet" {
 
   # HTTP access from anywhere
   ingress {
-    from_port = 80
-    to_port   = 80
-    protocol  = "tcp"
-    cidr_blocks = [
-      "0.0.0.0/0"
-    ]
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["198.162.0.0/16"]
   }
   egress {
     from_port   = 0
